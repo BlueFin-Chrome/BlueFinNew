1fe53c2380b6eab2fcb0e852e83eb1d2a60fcae2
hamza maqbool <hamzamaqbool22@gmail.com>
2024-06-19 10:26:52 +0500

    chore: Update Initiate component layout for better positioning of elements

--- src/components/WorkingScreen/Initiate.tsx
+++ src/components/WorkingScreen/Initiate.tsx
@@ -9,7 +9,7 @@ const Initiate = ({ setShowWorkingScreen }) => {
   return (
     <div className="fixed top-0 right-0 w-[751px] h-[905px]">
       <div className="w-full h-full bg-white border-l border-gray-200 shadow-md">
-        <div className="flex flex-col items-center gap-9 absolute w-[349px] h-[410px] left-[201px] top-1/2 transform -translate-y-1/2">
+        <div className="flex flex-col items-center gap-9 absolute w-[349px] h-[410px] left-[201px] top-[40%] transform -translate-y-1/2">
           <div className="flex flex-col items-center w-[349px] h-[259px]">
             <div className="w-[290px] h-[217px] bg-cover">
               {/* IMG png is in root/assets/house.png */}
--- src/contents/index.tsx
+++ src/contents/index.tsx
@@ -40,9 +40,9 @@ const PlasmoOverlay = () => {
         <div
           className="flex justify-end w-screen relative top-[120px] cursor-pointer"
           onClick={handleFilter}>
-          <div className="flex text-[16px] relative right-[18px] hover:bg-[#194185] bg-white  hover:text-white text-black  font-semibold w-[190px] h-[35px] rounded-lg py-1 px-5">
+          <div className="flex text-[16px] relative right-[18px] border border-[#D1D1D1] hover:bg-[#194185] bg-white  hover:text-white text-black  font-semibold w-[190px] h-[35px] rounded-lg py-1 px-5">
             <div className="flex justify-between space-x-2">
-              <BlueFinnLogo />
+              <BlueFinnLogo className="relative bottom-0.5 " />
               <h1>Filter for Deals</h1>
             </div>
           </div>
